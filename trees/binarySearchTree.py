1860f600ed8e5f1b7cc078771247923b9dcff5b7
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-01 11:26:08 +0530

    Binary Tree with Traversals

--- trees/binarySearchTree.py
+++ trees/binarySearchTree.py
@@ -16,4 +16,40 @@ class BinarySearchTree:
             else:
                 self.right.insert(value)
 
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traversal()
 
+    def preorder_traversal(self):
+        print(self.value)
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
+
+    def postorder_traversal(self):
+        if self.left:
+            self.left.postorder_traversal()
+        if self.right:
+            self.right.postorder_traversal()
+        print(self.value)
+
+tree = BinarySearchTree(6)
+tree.insert(5)
+tree.insert(2)
+tree.insert(4)
+tree.insert(1)
+tree.insert(2)
+tree.insert(4)
+tree.insert(19)
+tree.insert(29)
+tree.insert(11)
+tree.insert(4)
+tree.insert(2)
+
+# tree.inorder_traversal()
+# tree.preorder_traversal()
+tree.postorder_traversal()
