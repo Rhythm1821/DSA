8a129da19f2201dbb73ad9aba78a721dd9a14f66
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-01 12:13:05 +0530

    find element feature added

--- trees/binarySearchTree.py
+++ trees/binarySearchTree.py
@@ -3,18 +3,21 @@ class BinarySearchTree:
         self.value=value
         self.right=None
         self.left=None
+        self.data=None
 
-    def insert(self,value):
+    def insert(self,value,data=None):
         if self.value>value:
             if self.left is None:
                 self.left= BinarySearchTree(value)
+                self.left.data = data
             else:
-                self.left.insert(value)
+                self.left.insert(value,data)
         else:
             if self.right is None:
                 self.right=BinarySearchTree(value)
+                self.right.data = data
             else:
-                self.right.insert(value)
+                self.right.insert(value,data)
 
     def inorder_traversal(self):
         if self.left:
@@ -36,12 +39,26 @@ class BinarySearchTree:
         if self.right:
             self.right.postorder_traversal()
         print(self.value)
+    
+    def find(self,value):
+        if value < self.value:
+            if self.left is None:
+                return None
+            else:
+                return self.left.find(value)
+        elif value > self.value:
+            if self.right is None:
+                return None
+            else:
+                return self.right.find(value)
+        else:
+            return self
 
 tree = BinarySearchTree(6)
 tree.insert(5)
 tree.insert(2)
 tree.insert(4)
-tree.insert(1)
+tree.insert(1,{"data":45})
 tree.insert(2)
 tree.insert(4)
 tree.insert(19)
@@ -52,4 +69,6 @@ tree.insert(2)
 
 # tree.inorder_traversal()
 # tree.preorder_traversal()
-tree.postorder_traversal()
+# tree.postorder_traversal()
+
+print(tree.find(1).data["data"])
