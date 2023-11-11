2f10211493db4cfa7d8859fc4847b491874d2323
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-11 11:25:36 +0530

    linkedList complete

--- linkedList/singlyLinkedList.py
+++ linkedList/singlyLinkedList.py
@@ -49,9 +49,46 @@ class SLL:
             while temp.next is not None:
                 temp = temp.next
             temp.next=None
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            if self.start.item==data:
+                self.start=None
+        else:
+            temp=self.start
+            if temp.item==data:
+                self.start=temp.next
+            else:
+                while temp.next is not None:
+                    if temp.next.item==data:
+                        temp.next=temp.next.next
+                        break
+                    temp=temp.next
+                    
+    def __iter__(self):
+        return SLLIterator(self.start)
+
+class SLLIterator:
+    def __init__(self,start):
+        self.current=start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self.current:
+            raise StopIteration
+        data=self.current.item
+        self.current=self.current.next
+        return data
 
 mylist=SLL()            
 mylist.insert_at_start(10)
 mylist.insert_at_last(100)
 mylist.insert_after(mylist.search(10),50)
-mylist.print_list()
+# mylist.print_list()
+# mylist.delete_item(50)
+for i in iter(mylist):
+    print(i)
+# print()
+# mylist.print_list()
+# print
