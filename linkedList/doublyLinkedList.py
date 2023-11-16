1ac3dc66524623174d3258e985b26a315584a7e0
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-16 11:21:10 +0530

    delete first, last and item plus iterator

--- linkedList/doublyLinkedList.py
+++ linkedList/doublyLinkedList.py
@@ -5,7 +5,7 @@ class Node:
         self.next=next
 
 class DLL:
-    def __init__(self,start):
+    def __init__(self,start=None):
         self.start=start
     def is_empty(self):
         return self.start==None
@@ -13,12 +13,16 @@ class DLL:
         n=Node(None,data,self.start)
         if not self.is_empty():
             self.start.prev=n
+            # self.start.prev=n
         self.start=n
     def insert_at_last(self,data):
         temp=self.start
-        if self.start is not None:
-            while temp.next is not None:
+        if self.start!=None:
+            while temp.next!=None:
                 temp=temp.next
+        # if self.start is not None:
+        #     while temp.next is not None:
+        #         temp=temp.next
         n=Node(temp,data,None)
         if temp==None:
             self.start=n
@@ -26,10 +30,75 @@ class DLL:
             temp.next=n
         
     def search(self,data):
-        if not self.is_empty():
+        temp=self.start
+        while temp.next is not None:
+            if temp==data:
+                return temp
+            temp=temp.next
+        return None
+    
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n=Node(temp,data,temp.next)
+            if temp.next is not None:
+                temp.next.prev=n
+            temp.next=n
+    def print_list(self):
+        temp=self.start
+        while temp.next is not None:
+            print(temp)
+            temp.next
+    def delete_first(self):
+        if self.start is not None:
+            if self.start.next is not None:
+                self.start=self.start.next
+                self.start.prev=None
+            else:
+                self.start=None
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start=None
+        else:
             temp=self.start
             while temp.next is not None:
-                if temp==data:
-                    return temp
                 temp=temp.next
-            return None
+            temp.prev.next=None
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        else:
+            temp=self.start
+            while temp is not None:
+                if temp.item==data:
+                    if temp.next is not None:
+                        temp.next.prev=temp.prev
+                    if temp.prev is not None:
+                        temp.prev.next=temp.next
+                    else:
+                        self.start=temp.next
+                    break
+    
+    def __iter__(self):
+        return DLLIterator(self.start)
+    
+class DLLIterator:
+    def __init__(self,start):
+        self.current=start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self.current:
+            raise StopIteration
+        data=self.current.item
+        self.current=self.current.next
+        return data
+
+dll = DLL()
+dll.insert_at_start(10)
+dll.insert_at_last(20)
+dll.insert_after(dll.search(10),15)
+
+for x in dll:
+    print(x)
