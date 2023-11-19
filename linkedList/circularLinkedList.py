2718b3fae90a9548a76ee0726ef49a05d4c047d3
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-19 11:43:15 +0530

    search item, insert after data, print list

--- linkedList/circularLinkedList.py
+++ linkedList/circularLinkedList.py
@@ -26,4 +26,32 @@ class CLL:
                 self.last.next=n
                 self.last=n
     def search(self,data):
+        if self.is_empty():
+            return None
+        temp=self.last.next    
+        while temp!=self.last:
+            if temp==data:
+                return temp
+            temp=temp.next
+        if temp.item==data:
+            return temp
+        return None
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n=Node(data,temp.next)
+            temp.next=n
+            if temp==self.last:
+                self.last=n
+    def print_list(self):
+        if not self.is_empty():
+            temp=self.last.next
+            while temp!=self.last:
+                print(temp.next,end=" ")
+                temp=temp.next
+            print(temp.item)
+    def delete_first(self):
+        pass
+    def delete_last(self):
+        pass
+    def delete_item(self):
         pass
