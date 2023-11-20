a34444b76b272df3195d39c0b224e88015b4c727
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-20 11:52:19 +0530

    circular linked list complete

--- linkedList/circularLinkedList.py
+++ linkedList/circularLinkedList.py
@@ -4,7 +4,7 @@ class Node:
         self.next=next
 
 class CLL:
-    def __init__(self,last):
+    def __init__(self,last=None):
         self.last=last
     def is_empty(self):
         return self.last==None
@@ -30,7 +30,7 @@ class CLL:
             return None
         temp=self.last.next    
         while temp!=self.last:
-            if temp==data:
+            if temp.item==data:
                 return temp
             temp=temp.next
         if temp.item==data:
@@ -46,12 +46,76 @@ class CLL:
         if not self.is_empty():
             temp=self.last.next
             while temp!=self.last:
-                print(temp.next,end=" ")
+                print(temp.item,end=" ")
                 temp=temp.next
             print(temp.item)
     def delete_first(self):
-        pass
+        if not self.is_empty():
+            if self.last.next is None:
+                self.last=None
+            else:
+                self.last.next = self.last.next.next
     def delete_last(self):
-        pass
-    def delete_item(self):
-        pass
+        if not self.is_empty():
+            temp=self.last.next
+            if self.last.next is None:
+                self.last=None
+            else:
+                while temp.next!=self.last:
+                    temp=temp.next
+                temp.next=self.last.next
+                self.last=temp
+    def delete_item(self,data):
+        if not self.is_empty():
+            if self.last.next==self.last:
+                if self.last.item==data:
+                    self.last=None
+            else:
+                if self.last.next.item==data:
+                    self.delete_first()
+                else:
+                    temp=self.last.next
+                    while temp==self.last:
+                        if temp.next==self.last:
+                            if self.last.item==data:
+                                self.delete_last()
+                            break
+                        if temp.next.item==data:
+                            temp.next=temp.next.next
+                            break
+                        temp=temp.next
+    def __iter__(self):
+        return CLLIterator(None) if self.last==None else CLLIterator(self.last.next)
+
+class CLLIterator:
+    def __init__(self,start):
+        self.current=start
+        self.start=start
+        self.count=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current==None:
+            raise StopIteration
+        if self.current==self.start and self.count==1:
+            raise StopIteration
+        else:
+            self.count=1
+        data=self.current.item
+        self.current=self.current.next
+        return data
+    
+
+cll = CLL()
+cll.insert_at_start(10)
+cll.insert_at_start(20)
+cll.insert_at_last(30)
+cll.insert_at_last(40)
+cll.insert_after(cll.search(10),50)
+
+# for i in cll:
+#     print(i,end=" ")
+
+cll.print_list()
+
+# Output- 20 10 50 30 40
