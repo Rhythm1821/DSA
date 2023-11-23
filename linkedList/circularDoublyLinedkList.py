d6490333697880bcecf66eefe2cc80fe44f191d4
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-23 11:51:53 +0530

    search, insert_after, print_list, delete_first,delete_last

--- linkedList/circularDoublyLinedkList.py
+++ linkedList/circularDoublyLinedkList.py
@@ -33,3 +33,53 @@ class CDLL:
             n.next=self.start
             self.start.prev.next=n
             self.start.prev=n
+
+    def search(self,data):
+        temp=self.start
+        if self.is_empty():
+            return None
+        if temp.item==data:
+            return temp
+        else:
+            temp=temp.next 
+        while temp is not None:
+            if temp.item==data:
+                return temp
+            temp=temp.next
+        return None
+    
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n=Node(data)
+            n.next=temp.next
+            n.prev=temp
+            temp.next.prev=n
+            temp.next=n
+    def print_list(self):
+        temp=self.start
+        if temp is not None:
+            print(temp.item,end=" ")
+            temp=temp.next
+            while temp is not self.start:
+                print(temp.item,end=" ")
+                temp=temp.next
+
+    def delete_first(self):
+        if not self.is_empty():
+            if self.start.next==self.start:
+                self.start=None
+            else:
+                self.start.prev.next=self.start.next
+                self.start.next.prev=self.start.prev
+                self.start=self.start.next
+    
+    def delete_last(self):
+        if not self.is_empty():
+            if self.start.next==self.start:
+                self.start=None
+            else:
+                self.start.prev.prev.next=self.start
+                self.start.prev=self.start.prev.prev
+
+    def delete_item(self,data):
+        pass          
