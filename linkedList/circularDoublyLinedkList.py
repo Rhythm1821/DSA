f08a88d183246cee79f9d1c7d7db33961247ce06
Rhythm1821 <alamrawat1821@gmail.com>
2023-11-24 12:22:41 +0530

    circular linked list complete

--- linkedList/circularDoublyLinedkList.py
+++ linkedList/circularDoublyLinedkList.py
@@ -1,52 +1,57 @@
 class Node:
-    def __init__(self,prev=None,item=None,next=None):
+    def __init__(self,item=None,prev=None,next=None):
         self.prev=prev
         self.item=item
         self.next=next
 
 class CDLL:
-    def __init__(self,start):
+    def __init__(self,start=None):
         self.start=start
 
     def is_empty(self):
         return self.start==None
     
-    def insert_at_start(self,data):
-        n=Node(data)
+    def insert_at_start(self, data):
+        n = Node(data)
         if self.is_empty():
-            n.next=n
-            n.prev=n
+            n.next = n
+            n.prev = n
         else:
-            n.next=self.start
-            n.prev=self.start.prev
-            self.start.prev.next=n
-            self.start.prev=n
-    
-    def insert_at_last(self,data):
-        n=Node(data)
-        if self.is_empty():
-            n.next=n
-            n.prev=n
-            self.start=n
-        else:
-            n.prev=self.start.prev
-            n.next=self.start
-            self.start.prev.next=n
-            self.start.prev=n
+            n.next = self.start
+            n.prev = self.start.prev
+            self.start.prev.next = n
+            self.start.prev = n
+        self.start = n
 
-    def search(self,data):
-        temp=self.start
+
+    def insert_at_last(self, data):
+        n = Node(data)
+        if self.is_empty():
+            n.next = n
+            n.prev = n
+            self.start = n
+        else:
+            n.next = self.start
+            n.prev = self.start.prev
+            n.prev.next = n
+            self.start.prev = n
+
+
+    def search(self, data):
+        temp = self.start
         if self.is_empty():
             return None
-        if temp.item==data:
+        if temp.item == data:
             return temp
         else:
-            temp=temp.next 
-        while temp is not None:
-            if temp.item==data:
+            temp = temp.next
+
+        while temp != self.start:
+            if temp.item == data:
                 return temp
-            temp=temp.next
+            temp = temp.next
         return None
+
     
     def insert_after(self,temp,data):
         if temp is not None:
@@ -82,4 +87,48 @@ class CDLL:
                 self.start.prev=self.start.prev.prev
 
     def delete_item(self,data):
-        pass          
+        if self.start is not None:
+            temp=self.start
+            if temp.item==data:
+                self.delete_first()
+            else:
+                while temp!=self.start:
+                    if temp.item==data:
+                        temp.prev.next=temp.next
+                        temp.next.prev=temp.prev
+                    temp=temp.next
+
+    def __iter__(self):
+        return CDLLIterator(self.start)
+    
+class CDLLIterator:
+    def __init__(self,start):
+        self.current=start
+        self.start=start
+        self.count=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration 
+        if self.current==self.start and self.count==1:
+            raise StopIteration
+        else:
+            self.count=1
+        
+        data= self.current.item
+        self.current=self.current.next
+        return data
+
+mylist=CDLL()
+mylist.insert_at_start(10)
+mylist.insert_at_last(20)
+mylist.insert_at_last(30)
+mylist.insert_at_last(40)
+mylist.insert_after(mylist.search(30),35)
+
+for i in mylist:
+    print(i,end=" ")
+print()
+
+# mylist.print_list()
