6073c58d9c4731a8dc0ea5a8a62c5e2e9ccae343
Rhythm1821 <alamrawat1821@gmail.com>
2024-06-01 14:59:42 +0530

    priority queue using linked list

--- queue/queue.py
+++ queue/queue.py
@@ -313,3 +313,54 @@ p1.push("Ambika",1)
 while not p1.is_empty():
     print(p1.pop())
 """
+
+# Priority Queue using Linked List
+"""
+class Node:
+    def __init__(self,item=None,priority=None,next=None):
+        self.item=item
+        self.priority = priority
+        self.next=next
+    
+class PriorityQueueLinkedList:
+    def __init__(self):
+        self.start=None
+        self.count=0
+    def is_empty(self):
+        return self.start==None
+    
+    def push(self,data,priority):
+        n=Node(data,priority)
+        if not self.start or priority<self.start.priority:
+            n.next=self.start
+            self.start=n
+        else:
+            temp=self.start
+            while temp.next and temp.next.priority<priority:
+                temp=temp.next
+            n.next=temp.next
+            temp.next=n
+        self.count+=1
+    
+    def pop(self):
+        if self.is_empty():
+            raise "Queue is empty"
+        data=self.start.item
+        self.start=self.start.next
+        self.count-=1
+        return data
+    
+    def size(self):
+        return self.count
+    
+p1=PriorityQueueLinkedList()
+p1.push("Amit",4)
+p1.push("Arjun",7)
+p1.push("Ashima",2)
+p1.push("Agrah",5)
+p1.push("Anant",8)
+p1.push("Ambika",1)
+
+while not p1.is_empty():
+    print(p1.pop())
+"""
